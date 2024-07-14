ecfe141ba4f1573e8c5e514d8a1b47b4e776f47d
Tomás Flores Alvear <tomas.flores.a@gmail.com>
2024-07-14 19:49:41 -0400

    chore: removes unused structs

--- interfaces/http/echo/middleware/set_session_in_context.go
+++ interfaces/http/echo/middleware/set_session_in_context.go
@@ -1,22 +1,37 @@
 package middleware
 
-import "github.com/labstack/echo/v4"
+import (
+	"github.com/labstack/echo/v4"
+	"log"
+)
+
+const (
+	SessionHeader     = "Session"
+	RequestSessionKey = "requestSession"
+)
 
 func SetSessionInContext() echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
-			// First, look in the request header for the Authorization key
-			session := c.Request().Header.Get("Session")
+			// Attempt to get the session from the request header first
+			session := c.Request().Header.Get(SessionHeader)
 
-			// If no present, look in cookie
+			// If not present, attempt to get it from the cookie
 			if session == "" {
-				cookie, err := c.Cookie("Session")
-				if err == nil {
-					session = cookie.Value
+				cookie, err := c.Cookie(SessionHeader)
+				if err != nil {
+					if err.Error() != "http: named cookie not present" {
+						// Log unexpected errors
+						log.Printf("Error retrieving session cookie: %v", err)
+					}
+					// Proceed to the next middleware if the cookie is not present or an error occurred
+					return next(c)
 				}
+				session = cookie.Value
 			}
 
-			c.Set("requestSession", session)
+			// Set the session in the context
+			c.Set(RequestSessionKey, session)
 			return next(c)
 		}
 	}
--- interfaces/http/echo/middleware/set_token_in_context.go
+++ interfaces/http/echo/middleware/set_token_in_context.go
@@ -1,23 +1,31 @@
 package middleware
 
-import "github.com/labstack/echo/v4"
+import (
+	"errors"
+	"github.com/labstack/echo/v4"
+	"log"
+	"net/http"
+)
 
 const (
-	TokenKey = "requestToken"
+	TokenKey      = "requestToken"
+	Authorization = "Authorization"
 )
 
 func SetTokenInContext() echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
-			// First, look in the request header for the Authorization key
-			token := c.Request().Header.Get("Authorization")
+			token := c.Request().Header.Get(Authorization)
 
-			// If no present, look in cookie
 			if token == "" {
-				cookie, err := c.Cookie("Authorization")
-				if err == nil {
-					token = cookie.Value
+				cookie, err := c.Cookie(Authorization)
+				if err != nil {
+					if !errors.Is(err, http.ErrNoCookie) {
+						log.Printf("Error retrieving authorization cookie: %v", err)
+					}
+					return next(c)
 				}
+				token = cookie.Value
 			}
 
 			c.Set(TokenKey, token)
