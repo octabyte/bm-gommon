f00812b203dda677e34be6193cdc385d96e53a01
Tomás Flores Alvear <tomas.flores.a@gmail.com>
2024-07-27 12:19:25 -0400

    feat: adds logger

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/labstack/echo/v4 v4.11.1
 	github.com/labstack/gommon v0.4.0
 	github.com/tidwall/gjson v1.17.1
+	go.uber.org/zap v1.27.0
 	golang.org/x/net v0.12.0
 )
 
@@ -18,6 +19,7 @@ require (
 	github.com/tidwall/pretty v1.2.1 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
 	golang.org/x/crypto v0.11.0 // indirect
 	golang.org/x/sys v0.12.0 // indirect
 	golang.org/x/text v0.11.0 // indirect
---
+++ utils/logger/logger.go
@@ -0,0 +1,107 @@
+package logger
+
+import (
+	"fmt"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"os"
+)
+
+func Init(level string) {
+	encoderCfg := zap.NewProductionEncoderConfig()
+	encoderCfg.TimeKey = "timestamp"
+	encoderCfg.EncodeTime = zapcore.ISO8601TimeEncoder
+
+	config := zap.Config{
+		Level:             zap.NewAtomicLevelAt(getLogLevelFromString(level)),
+		Development:       false,
+		DisableCaller:     false,
+		DisableStacktrace: false,
+		Sampling:          nil,
+		Encoding:          "json",
+		EncoderConfig:     encoderCfg,
+		OutputPaths: []string{
+			"stdout",
+		},
+		ErrorOutputPaths: []string{
+			"stderr",
+		},
+		InitialFields: map[string]interface{}{
+			"pid": os.Getpid(),
+		},
+	}
+	zap.ReplaceGlobals(zap.Must(config.Build()))
+}
+
+func LogDebug(msg string, fields ...zap.Field) {
+	zap.L().Debug(msg, fields...)
+}
+
+func LogDebugf(msg string, args ...interface{}) {
+	fmtdMsg := fmt.Sprintf(msg, args...)
+	zap.L().Debug(fmtdMsg)
+}
+
+func LogInfo(msg string, fields ...zap.Field) {
+	zap.L().Info(msg, fields...)
+}
+
+func LogInfof(msg string, args ...interface{}) {
+	fmtdMsg := fmt.Sprintf(msg, args...)
+	zap.L().Info(fmtdMsg)
+}
+
+func LogWarn(msg string, fields ...zap.Field) {
+	zap.L().Warn(msg, fields...)
+}
+
+func LogWarnf(msg string, args ...interface{}) {
+	fmtdMsg := fmt.Sprintf(msg, args...)
+	zap.L().Warn(fmtdMsg)
+}
+
+func LogError(msg string, fields ...zap.Field) {
+	zap.L().Error(msg, fields...)
+}
+
+func LogErrorf(msg string, args ...interface{}) {
+	fmtdMsg := fmt.Sprintf(msg, args...)
+	zap.L().Error(fmtdMsg)
+}
+
+func LogFatal(msg string, fields ...zap.Field) {
+	zap.L().Fatal(msg, fields...)
+}
+
+func LogFatalf(msg string, args ...interface{}) {
+	fmtdMsg := fmt.Sprintf(msg, args...)
+	zap.L().Fatal(fmtdMsg)
+}
+
+func LogPanic(msg string, fields ...zap.Field) {
+	zap.L().Panic(msg, fields...)
+}
+
+func LogPanicf(msg string, args ...interface{}) {
+	fmtdMsg := fmt.Sprintf(msg, args...)
+	zap.L().Panic(fmtdMsg)
+}
+
+func getLogLevelFromString(level string) zapcore.Level {
+	switch level {
+	case "debug":
+		return zapcore.DebugLevel
+	case "info":
+		return zapcore.InfoLevel
+	case "warn":
+		return zapcore.WarnLevel
+	case "error":
+		return zapcore.ErrorLevel
+	default:
+		return zapcore.InfoLevel
+	}
+}
+
+func Sync() {
+	_ = zap.L().Sync()
+}
