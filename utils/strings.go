5bdb3e44f41c00db8948d8cbd5dab436ef6215b1
Tomás Flores Alvear <tomas.flores.a@gmail.com>
2024-09-05 23:56:13 -0400

    feat: adds helper for strings

--- utils/strings.go
+++ utils/strings.go
@@ -5,3 +5,7 @@ import "github.com/goccy/go-json"
 func StructToBytes(s interface{}) ([]byte, error) {
 	return json.Marshal(s)
 }
+
+func BytesToStruct(data []byte, s interface{}) error {
+	return json.Unmarshal(data, s)
+}
