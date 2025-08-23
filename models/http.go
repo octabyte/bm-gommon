bb0ed5a3d0359acc9bc342442904518878c3f4fa
tomtomtom <tomas.flores.a@gmail.com>
2025-08-23 14:53:15 -0400

    feat: Adds http models

---
+++ models/http.go
@@ -0,0 +1,42 @@
+package models
+
+import (
+	"github.com/labstack/echo/v4"
+)
+
+type Error struct {
+	Code  int    `json:"code"`
+	Error string `json:"error"`
+}
+
+type Data struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+	Data    any    `json:"data"`
+}
+
+func Response(c echo.Context, statusCode int, data any) error {
+	return c.JSON(statusCode, data)
+}
+
+func ResponseWithData(c echo.Context, statusCode int, message string, data any) error {
+	return Response(c, statusCode, Data{
+		Code:    statusCode,
+		Message: message,
+		Data:    data,
+	})
+}
+
+func MessageResponse(c echo.Context, statusCode int, message string) error {
+	return Response(c, statusCode, Data{
+		Code:    statusCode,
+		Message: message,
+	})
+}
+
+func ErrorResponse(c echo.Context, statusCode int, message string) error {
+	return Response(c, statusCode, Error{
+		Code:  statusCode,
+		Error: message,
+	})
+}
